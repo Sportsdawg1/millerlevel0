fa4521b68fe9dd9a77b9a9dea7c3a86d383822ec
NY-Giants <league@WTS10.attlocal.net>
2016-12-13 18:22:46 -0800

    ASDFGHJKL;'

---
+++ src/millerlevel0/FindYourAdventure.java
@@ -0,0 +1,18 @@
+package millerlevel0;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class FindYourAdventure {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		while(true) {
+		String command = scanner.nextLine();
+		System.out.println(command);
+		}
+		JOptionPane.show
+	}
+
+}
